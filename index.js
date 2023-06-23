3f596cbdb068d5c852e03d1328b60a98420fa293
Jake Bogan <jakebogan01@gmail.com>
2023-06-23 12:33:21 -0400

    adds multer and image upload functions

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const dotenv = require("dotenv");
 const express = require("express");
 // Initialize mongoose
 const mongoose = require("mongoose");
+// Initialize multer
+const multer = require("multer");
+// Initialize path
+const path = require("path");
 // Import routes
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 // express app
 const app = express();
+app.use('/images', express.static('images'));
 dotenv.config();
 
 // cors
@@ -22,6 +27,31 @@ app.use(
 );
 
 // middleware
+// image storage
+const storage = multer.diskStorage({
+     destination: (req, file, cb) => {
+          cb(null, 'images');
+     },
+     filename: (req, file, cb) => {
+          console.log(file);
+          cb(null, Date.now() + path.extname(file.originalname));
+     }
+});
+// filter image extensions
+const fileFilter = (req, file, cb) => {
+     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+          cb(null, true);
+     } else {
+          cb(null, false);
+     }
+}
+const upload=multer({
+     storage: storage,
+     limits: {
+          fileSize:1024*1024*5
+     },
+     fileFilter: fileFilter
+});
 // this will parse the request body and add it to req.body
 app.use(express.json());
 app.use((req, res, next) => {
@@ -35,6 +65,17 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
+// upload image
+app.post("/api/upload", upload.single("image"), (req, res) => {
+     if (!req.file) {
+          return res.status(400).send("you didn't upload image (jpeg OR png)")
+     }
+     return res.send({message: 'Image has been uploaded!', image: req.file.filename});
+});
+// retrieve image
+app.get("/api/images/:image", (req, res) => {
+     res.sendFile(path.join(__dirname, "images", req.params.image));
+});
 
 // connect to mongodb to database
 mongoose
